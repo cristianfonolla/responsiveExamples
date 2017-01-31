e8aa5b24f8623b0da06d8c3786714f4c0914dfba
cristian <cristian.fonolla@gmail.com>
2017-01-31 16:39:37 +0100

    added style weather

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <weather></weather>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import weather from './components/weather'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    weather
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  body {
+
+    font-family: Roboto, Arial;
+    font-size: 1.2em;
+    color: #888;
+
+  }
 </style>
---
+++ src/components/weather.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="weather">
+
+    <div class="location">{{location}}</div>
+    <div class="date">{{date}}</div>
+    <div class="description">{{description}}</div>
+    <div class="current">
+      <div class="visual">
+        <div class="cloud">cloud</div>
+        <div class="temp">{{temp}}</div>
+        <div class="scale">º</div>
+      </div>
+      <div class="text">
+        <div class="precipitation">Precipitation: {{precipitation}}</div>
+        <div class="humidity">humidity: {{humidity}}</div>
+        <div class="wind">wind: {{wind}}</div>
+        <div class="pollen">pollen: {{pollen}}</div>
+      </div>
+    </div>
+    <div class="forecast">
+      <div class="forecast-day" v-for="forecast in forecasts">
+        <div class="date">{{forecast.date}}</div>
+        <div class="date">{{forecast.icon}}</div>
+        <div class="high-temp">{{forecast.hightemp}}</div>
+        <div class="low-temp">{{forecast.lowtemp}}</div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather',
+  data () {
+    return {
+      location: 'NY City',
+      date: 'Tursday 36 apa',
+      description: 'Desc here',
+      temp: 25,
+      precipitation: '100%',
+      humidity: '97%',
+      wind: '4mph SW',
+      pollen: 36,
+      forecasts: [
+        {'date': 'Monday', 'icon': 'cloud', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Tuesday', 'icon': 'sun', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Wednesday', 'icon': 'sun-cloud', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Thursday', 'icon': 'raining', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Friday', 'icon': 'raining', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Saturday', 'icon': 'raining', 'lowtemp': 28, 'hightemp': 45},
+        {'date': 'Sunday', 'icon': 'raining', 'lowemp': 28, 'hightemp': 45}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
